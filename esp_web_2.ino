daf985b986e0bda2dd3d80e8933bcce52633ff6e
MartinAdytia <117578911+MartinAdytia@users.noreply.github.com>
2022-12-05 18:33:54 +0700

    Update esp_web_2.ino

--- esp_web_2.ino
+++ esp_web_2.ino
@@ -8,8 +8,8 @@
 
 
 // Replace with your network credentials
-const char* ssid     = "BAYUCARAKA";
-const char* password = "garudakujayaselalu";
+const char* ssid     = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
 
 // Set web server port number to 80
 WiFiServer server(80);
